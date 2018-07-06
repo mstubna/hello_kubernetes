c5d9416d442178fda2ed8661a5cd2e65c33d6931
Mike Stubna <mike@stubna.com>
2018-07-06 08:35:38 -0400

    fix spec

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -9,6 +9,6 @@ RSpec.describe Widget, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 end
